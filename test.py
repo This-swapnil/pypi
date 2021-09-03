f548c53f7f6398a4d99cbdf0c93039d08af6bafb
swapnil sonawane <sswapnil0098@gmail.com>
2021-09-03 13:44:21 +0530

    fourth commit

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-from clean import preprocessing
-import pandas as pd
+# from preprocessing.clean import preprocessing
+# import pandas as pd
 
-df = pd.read_csv("Advertising.csv")
-print(df.head())
+# df = pd.read_csv("Advertising.csv")
+# print(df.head())
 
-pre = preprocessing()
-print(pre.check_na(df))
-print(pre.fill_na(df))
-pre.fill_na(df)
-print(pre.drop_col(df, ["TV", "radio"], inplace=True))
-print(pre.drop_missing(df, 0, inplace=True))
-print(pre.standard_scaler(df))
-print(pre.normalize(df))
-print(pre.show_data(df))
+# pre = preprocessing()
+# print(pre.check_na(df))
+# print(pre.fill_na(df))
+# pre.fill_na(df)
+# print(pre.drop_col(df, ["TV", "radio"], inplace=True))
+# print(pre.drop_missing(df, 0, inplace=True))
+# print(pre.standard_scaler(df))
+# print(pre.normalize(df))
+# print(pre.show_data(df))
